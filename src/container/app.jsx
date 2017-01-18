403ec07e2d48ab025e3deca75b503ca46a974186
Yuval Saraf <yuvalsaraf@gmail.com>
2017-01-18 18:16:18 +0200

    Updated all dep, webpack 2

--- src/container/app.jsx
+++ src/container/app.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import Header from 'src/component/header.jsx';
-
-injectTapEventPlugin(); // Needed for onTouchTap
 
 export default class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './container/app.jsx';
 import './style.css';
+
+injectTapEventPlugin(); // Needed for onTouchTap
 
 window.onload = () => {
   const div = document.getElementById('app');
